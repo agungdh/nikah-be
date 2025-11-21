673d2d99822fe0e8ca9937d054b8ff531f5efc0b
AgungDH <agungdh@live.com>
2025-11-21 10:33:24 +0700

    init

---
+++ src/main/java/id/my/agungdh/nikahbe/NikahBeApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.nikahbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikahBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NikahBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/nikahbe/NikahBeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.nikahbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikahBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

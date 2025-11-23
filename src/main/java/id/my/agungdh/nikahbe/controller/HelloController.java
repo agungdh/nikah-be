21823251af451710c2d38e40fca4d028a5e7078c
AgungDH <agungdh@live.com>
2025-11-23 18:25:33 +0700

    feat: log current thread info and return thread details in HelloController
    - Add SLF4J logger to HelloController
    - Log current thread and its class on /hello request
    - Change response to return the current thread string representation

--- src/main/java/id/my/agungdh/nikahbe/controller/HelloController.java
+++ src/main/java/id/my/agungdh/nikahbe/controller/HelloController.java
@@ -1,5 +1,7 @@
 package id.my.agungdh.nikahbe.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("hello")
 public class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping
     public String hello() {
-        return "Hello World";
+        Thread t = Thread.currentThread();
+        log.info(">> Current thread: {} (class={})", t, t.getClass());
+        return t.toString();
     }
 }
